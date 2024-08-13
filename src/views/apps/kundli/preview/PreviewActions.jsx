3b49035dae1a4b5f0bbbacc6c786c82b2e42440c
Vivek Lakhlani <viveklakhlani123@gmail.com>
2024-08-13 18:28:17 +0530

    after merge commit 13th August 2024 6:28 PM

---
+++ src/views/apps/kundli/preview/PreviewActions.jsx
@@ -0,0 +1,42 @@
+// React Imports
+import { useState } from 'react'
+
+// Next Imports
+import Link from 'next/link'
+import { useParams } from 'next/navigation'
+
+// MUI Imports
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Button from '@mui/material/Button'
+
+// Component Imports
+
+const PreviewActions = ({ onButtonClick }) => {
+
+  return (
+    <>
+      <Card>
+        <CardContent className='flex flex-col gap-4'>
+        <Button
+            fullWidth
+            variant='contained'
+            className='capitalize'
+            startIcon={<i className='tabler-download' />}
+            onClick={onButtonClick}
+          >
+            Print
+          </Button>
+          {/* <div className='flex items-center gap-4'>
+            <Button fullWidth color='secondary' variant='tonal' className='capitalize' onClick={onButtonClick}>
+              Print
+            </Button>
+            
+          </div> */}
+        </CardContent>
+      </Card>
+      </>
+  )
+}
+
+export default PreviewActions
