d37433042d790251c7a167822206b8d029a22a20
DhruviRana09 <Dhruvi.Rana@growthaxl.com>
2025-05-24 13:38:19 +0530

    resolve tab group issue and remove console from code 24-May-2025 01:10PM

--- src/assets/iconify-icons/bundle-icons-css.js
+++ src/assets/iconify-icons/bundle-icons-css.js
@@ -20,7 +20,7 @@ import { getIcons, getIconsCSS, stringToIcon } from '@iconify/utils'
 const sources = {
   json: [
     // Iconify JSON file (@iconify/json is a package name, /json/ is directory where files are, then filename)
-    require.resolve('@iconify/json/json/tabler.json')
+    // require.resolve('@iconify/json/json/tabler.json')
 
     // Custom file with only few icons
     /* {
