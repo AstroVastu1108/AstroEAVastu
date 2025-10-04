c05454c9f51ce4e7113f1f19c5e79a212002410b
Dhruvi Rana <dhruvirana567@gmail.com>
2025-10-04 20:06:49 +0530

    change in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,45 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   basePath: process.env.BASEPATH,
+//   serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+//   redirects: async () => {
+//     return [
+//       {
+//         source: '/',
+//         destination: '/login',
+//         permanent: true,
+//         locale: false
+//       }
+//     ]
+//   }
+// }
+
+// export default nextConfig
+
+
+import path from 'path';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: process.env.BASEPATH,
-  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
+  experimental: {
+    serverComponentsExternalPackages: ['@sparticuz/chromium', 'puppeteer-core'],
+  },
+  outputFileTracingIncludes: {
+    './src': [
+      path.resolve('./node_modules/@sparticuz/chromium/bin'),
+    ],
+  },
   redirects: async () => {
     return [
       {
         source: '/',
         destination: '/login',
         permanent: true,
-        locale: false
-      }
-    ]
-  }
-}
+        locale: false,
+      },
+    ];
+  },
+};
 
-export default nextConfig
+export default nextConfig;
